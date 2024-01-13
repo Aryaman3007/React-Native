433c726e13447184752cf922469087835e52d303
Aryaman30 <111583885+Aryaman3007@users.noreply.github.com>
2024-01-14 00:42:35 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -84,22 +84,6 @@ const styles = StyleSheet.create({
     borderColor: "#ebdcf4",
     justifyContent: 'center',
     alignItems: 'center',
-  },
-  left: {
-    position: 'absolute',
-    height:30,
-    width: 30,
-    borderTopRightRadius:30,
-    backgroundColor:'red',
-    top: 40,
-    left: -20,
-  },
-  right: {
-    position: 'absolute',
-    height:30,
-    width: 30,
-    borderTopLeftRadius:30,
-    backgroundColor:'red'
   }
 })
 
